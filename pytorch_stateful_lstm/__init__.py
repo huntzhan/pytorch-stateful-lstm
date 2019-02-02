b169ddb06019d34e4d904d5b6d85e5d08c1af6c2
huntzhan <huntzhan.dev@gmail.com>
2019-02-02 19:23:47 +0800

    Bump version: 1.3.2 → 1.3.3

--- pytorch_stateful_lstm/__init__.py
+++ pytorch_stateful_lstm/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Hunt Zhan"""
 __email__ = 'huntzhan.dev@gmail.com'
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 
 # To avoid `undefined symbol` error.
 import torch
--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cnt-dev/pytorch-stateful-lstm',
-    version='1.3.2',
+    version='1.3.3',
     zip_safe=False,
     # Pytorch Cpp Extension.
     ext_modules=[
